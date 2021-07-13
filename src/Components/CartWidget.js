f769864279affdbfc5ac53ec53ae4776fbe0c7f1
unknown <melinaglopez@hotmail.com>
2021-07-13 19:52:16 -0300

    text test

--- src/Components/CartWidget.js
+++ src/Components/CartWidget.js
@@ -3,11 +3,13 @@ import React from 'react'
 function CartWidget() {
      
     return (
-        <div class="image">
-            <img src="https://minicooperbmw.files.wordpress.com/2011/04/carrito-de-compras.png" alt="crt-img"></img>
+        <div class='cart-icon'>
+
+            <i class="fas fa-shopping-cart"></i>
 
         </div>
     );
    
-}
+    }   
+
 export default CartWidget
--- src/Components/ItemListContainer.js
+++ src/Components/ItemListContainer.js
@@ -5,7 +5,6 @@ function ItemListContainer(props) {
     return(
         <div>
             <h3 className= "container-fluid"> {props.greeting} </h3>
-            <h4>github solucionado</h4>
         </div>
     );
 }
